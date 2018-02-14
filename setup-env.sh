77ba99fec41cddf41ca93a09aee66b7699c80bb2
Raph Haddad <raph@haddad.id.au>
2018-02-14 11:11:31 +1100

    add generation of ssh key on machine

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing software..."
-sudo apt-get install -y git vim
+sudo apt-get install -y git vim xsel
 echo "Installing software...finished"
 
 setNumberVimFlag="set number"
@@ -10,3 +10,15 @@ if ! grep -q $setNumberVimFlag ~/.vimrc ; then
 else
 	echo "Vim settings already added"
 fi
+
+echo "remove existing ssh key if exists..."
+rm ~/.ssh/id_rsa
+rm ~/.ssh/id_rsa.pub
+echo "remove existing ssh key if exists...finished"
+
+echo "generate ssh key..."
+ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+cat ~/.ssh/id_rsa.pub  | xsel -ib
+echo "generate ssh key...finished"
+
+echo "public key is on clipboard"
