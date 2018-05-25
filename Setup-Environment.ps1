33dfae0b8df6df248192264d228153b839cd7102
Rapha <raphael@haddad.id.au>
2018-05-26 07:33:32 +1000

    Make choco install multiple lines and install treesize, everything, and slack

--- Setup-Environment.ps1
+++ Setup-Environment.ps1
@@ -1,7 +1,32 @@
 function Install-SoftwareUsingChoco() {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-    choco install googlechrome firefox visualstudiocode sql-server-management-studio resharper-platform linqpad git-credential-manager-for-windows gitextensions cmder nodejs.install greenshot poshgit screentogif kdiff3 docker azurestorageexplorer rdcman terraform virtualbox python f.lux keybase -y
+    choco install googlechrome `
+                  firefox `
+                  visualstudiocode `
+                  sql-server-management-studio `
+                  resharper-platform `
+                  linqpad `
+                  git-credential-manager-for-windows `
+                  gitextensions `
+                  cmder `
+                  nodejs.install `
+                  greenshot `
+                  poshgit `
+                  screentogif `
+                  kdiff3 `
+                  docker `
+                  azurestorageexplorer `
+                  rdcman `
+                  terraform `
+                  virtualbox `
+                  python `
+                  f.lux `
+                  keybase `
+                  treesizefree `
+                  everything `
+                  slack `
+                  -y
 }
 
 function Install-VsCodeExtentions() {
