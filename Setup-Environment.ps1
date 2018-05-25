d3a80e5ef21658ed121c6dba539aad70ad3f9b28
Raph <raph@haddad.id.au>
2018-05-25 15:27:53 +1000

    Add google drive

--- Setup-Environment.ps1
+++ Setup-Environment.ps1
@@ -2,7 +2,7 @@
 function Install-SoftwareUsingChoco() {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-    choco install googlechrome firefox visualstudiocode sql-server-management-studio resharper-platform linqpad git-credential-manager-for-windows gitextensions cmder nodejs.install greenshot poshgit screentogif kdiff3 docker azurestorageexplorer rdcman terraform virtualbox python f.lux keybase -y
+    choco install googlechrome googledrive firefox visualstudiocode sql-server-management-studio resharper-platform linqpad git-credential-manager-for-windows gitextensions cmder nodejs.install greenshot poshgit screentogif kdiff3 docker azurestorageexplorer rdcman terraform virtualbox python f.lux keybase -y
 }
 
 function Install-VsCodeExtentions() {
