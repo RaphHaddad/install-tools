d38c3cc9bce0337827f479f4753bc92263186abf
Rapha <raphael@haddad.id.au>
2019-10-22 16:09:29 +1100

    Fix README

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
-# To install your tools on a new Windows VM
-1. Install [Chocolatey](https://chocolatey.org/)
-2. Run this
-```
+# README
+
+## Windows
+
+```cmd
 .\Setup-Environment.ps1
 ```
-3. Install these things manually
 
-- [ ] Office365
-- [ ] Teams
-- [ ] Visual Studio
+Install these things manually
 
-# To install your tools on a Ubuntu 17.10 VM
-1. Run this
+[ ] Office365
+[ ] Teams
+[ ] Visual Studio
+
+## macOS
+
+```cmd
+./install-mac-tools.sh
 ```
+
+## Ubuntu
+
+```cmd
 sudo ./setup-env.sh
 sudo ./install-dotnet.sh
 sudo ./install-powershell.sh
 sudo ./install-docker-and-kubernetes.sh
 ```
-2. Enter SSH Key password when requested
+
+Enter SSH Key password when requested
--- install-mac-tools.sh
+++ install-mac-tools.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 echo install brew
 /usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)
@@ -8,16 +8,20 @@ echo installing tools via brew
 brew cask install adoptopenjdk
 
 brew install git \
-         git-credential-manager \
+             git-credential-manager \
              terraform
 
 brew cask install firefox \
           google-chrome \
           slack \
           dotnet-sdk \
-              visual-studio-code
+          visual-studio-code
 
 echo finished brew installs
+
+echo install git credential manager
+
+git-credential-manager install
 
 echo adding vs code to path
 
@@ -25,6 +29,8 @@ cat << EOF >> ~/.bash_profile
 # Add Visual Studio Code (code)
 export PATH=\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin
 EOF
+
+echo installing vs code extentions
 
 extentions=(ms-vscode.azure-account \ 
             ms-azuretools.vscode-azureterraform \
